7b5fe95cb1bce3cf42e1ef3fb4ea23bab9385f41
SavageLM <nagol.egavas@gmail.com>
2024-06-24 12:17:15 -0500

    Forgot to set the value of new_node

--- 1-binary_tree_insert_left.c
+++ 1-binary_tree_insert_left.c
@@ -21,6 +21,7 @@ binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value)
 		new_node->left = parent->left;
 		parent->left->parent = new_node;
 	}
+	new_node->n = value;
 	new_node->parent = parent;
 	parent->left = new_node;
 	return (new_node);
