ecc2c31475b8f8e82fcf127e8bf0b555b762073b
SavageLM <nagol.egavas@gmail.com>
2024-06-24 15:00:45 -0500

    Forgot to += instead of =

--- 11-binary_tree_size.c
+++ 11-binary_tree_size.c
@@ -12,7 +12,7 @@ size_t binary_tree_size(const binary_tree_t *tree)
 	if (!tree)
 		return (0);
 	size += 1;
-	size = binary_tree_size(tree->left);
-	size = binary_tree_size(tree->right);
+	size += binary_tree_size(tree->left);
+	size += binary_tree_size(tree->right);
 	return (size);
 }
