99e0c8eb233764867e0d8954d9959b4130114068
O-01 <sa.an-s@yandex.com>
2024-06-24 15:57:18 -0500

    Resolved naming issue in 14

--- 14-binary_tree_balance.c
+++ 14-binary_tree_balance.c
@@ -11,7 +11,7 @@ int binary_tree_balance(const binary_tree_t *tree)
 {
 	if (!tree)
 		return (0);
-	return (binary_tree_height(tree->left) - binary_tree_height(tree->right));
+	return (tree_height(tree->left) - tree_height(tree->right));
 }
 
 /**
