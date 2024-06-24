6c4b9775798f6ff6a7cd122df3213c0c8b05606c
SavageLM <nagol.egavas@gmail.com>
2024-06-24 12:34:00 -0500

    Called binary_tree_node onstead of mallocing a new node directly

--- 1-binary_tree_insert_left.c
+++ 1-binary_tree_insert_left.c
@@ -12,17 +12,15 @@ binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value)
 
 	if (!parent)
 		return (NULL);
-	new_node = malloc(sizeof(binary_tree_t));
+	new_node = binary_tree_node(parent, value);
 	if (!new_node)
 		return (NULL);
 
-	if (parent->left != NULL)
+	if (parent->left)
 	{
 		new_node->left = parent->left;
 		parent->left->parent = new_node;
 	}
-	new_node->n = value;
-	new_node->parent = parent;
 	parent->left = new_node;
 	return (new_node);
 }
