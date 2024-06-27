a238fda31a71e7fe2f2bbe67e604daeb717ec111
SavageLM <nagol.egavas@gmail.com>
2024-06-27 14:05:39 -0500

    Added code to file 122-array

--- 122-array_to_avl.c
+++ 122-array_to_avl.c
@@ -11,5 +11,14 @@ avl_t *array_to_avl(int *array, size_t size)
 	size_t iter = 0;
 	avl_t *root = NULL;
 
+	if (!array)
+		return (NULL);
+	if (!root)
+		root = binary_tree_node(NULL, array[iter]), iter++;
+
+	for (; iter < size; iter++)
+		if (!avl_insert(&root, array[iter]))
+			return (NULL);
+
 	return (root);
 }
