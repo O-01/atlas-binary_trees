71117b47a2bf7a4cc0bd4a1177d4bda97d6c426c
O-01 <sa.an-s@yandex.com>
2024-06-27 13:47:00 -0500

    Added test files for 122-124, added file for 122-array_to_avl - incomplete

---
+++ 122-array_to_avl.c
@@ -0,0 +1,15 @@
+#include "binary_trees.h"
+
+/**
+ * array_to_avl - builds AVL tree from array
+ * @array: pointer to first element of array to be converted
+ * @size: number of elements in array
+ * Return: pointer to root node of created AVL tree, NULL upon failure
+ */
+avl_t *array_to_avl(int *array, size_t size)
+{
+	size_t iter = 0;
+	avl_t *root = NULL;
+
+	return (root);
+}
