231e9ea6547eaa448bf474946f9d3d097ee03d36
SavageLM <nagol.egavas@gmail.com>
2024-07-03 14:25:21 -0500

    Fixed return statement

--- 113-bst_search.c
+++ 113-bst_search.c
@@ -13,7 +13,7 @@ bst_t *bst_search(const bst_t *tree, int value)
 	if (value < tree->n)
 		return bst_search(tree->left, value);
 	else if (value > tree->n)
-		return bst_search(tree->right, value);
+		return (bst_search(tree->right, value));
 	else if (value == tree->n)
 		return ((bst_t *)&tree->n);
 	return (NULL);
