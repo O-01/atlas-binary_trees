ccd1767afbfaa18e8e3775fe7cf1674727ed10b4
SavageLM <nagol.egavas@gmail.com>
2024-07-03 14:07:51 -0500

    Added file and code for bst_search

---
+++ 113-bst_search.c
@@ -0,0 +1,20 @@
+#include "binary_trees.h"
+
+/**
+ * bst_search - Search a tree for a value
+ * @tree: Tree to search
+ * @value: value to search for
+ * Return: Pointer to node with value, or NULL
+ */
+bst_t *bst_search(const bst_t *tree, int value)
+{
+	if (!tree)
+		return (NULL);
+	if (value < tree->n)
+		return (bst_search(tree->left, value));
+	else if (value > tree->n)
+		return (bst_search(tree->right, value));
+	else if (value == tree->n)
+		return (tree);
+	return (NULL);
+}
